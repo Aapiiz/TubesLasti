94da1a4a2d96b5bf7302f91d4d22368a7c5d4372
Aapiiz <18221167@std.stei.itb.ac.id>
2023-12-03 15:12:56 +0700

    instalasi tailwind

--- TubesLastiCoyyy/src/main.jsx
+++ TubesLastiCoyyy/src/main.jsx
@@ -1,10 +1,25 @@
+//import library
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import './index.css';
+import {createBrowserRouter, RouterProvider} from 'react-router-dom';
+
+//import pages
+
+
+const router = createBrowserRouter([
+  //menuju page login (pembeli)
+  {
+
+  },
+  //menuju page x
+  {
+
+  }
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
-)
+);
