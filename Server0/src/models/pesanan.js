b67b008840af8b18aca9f4f33f82e72799aab53d
Shulhan <18221051@std.stei.itb.ac.id>
2023-12-07 14:17:29 +0700

    perbaiki schema pesanan

--- Server0/src/models/pesanan.js
+++ Server0/src/models/pesanan.js
@@ -2,8 +2,8 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const Pesanan = new Schema({
-    nama: {
-        type: String,
+    nomormeja: {
+        type: Number,
         required: true
     },
     listmenu: {
