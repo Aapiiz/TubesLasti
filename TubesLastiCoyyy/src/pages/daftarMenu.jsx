63c7abe422f9240abe0ae299b428e70fee22145d
arifu11 <arifuddinachmad11@gmail.com>
2023-12-07 15:49:53 +0700

    Perbaiki daftar menu

--- TubesLastiCoyyy/src/pages/daftarMenu.jsx
+++ TubesLastiCoyyy/src/pages/daftarMenu.jsx
@@ -1,13 +1,13 @@
-// import Header from "../components/Header";
+import Header from "../components/Header";
 import ContentDaftarMenu from "../components/ContentDaftarMenu";
-// import Footer from "../components/Footer";
+import Footer from "../components/Footer";
 
 const DaftarMenuPage = () => {
     return (
         <div className="flex flex-col min-h-screen bg-[#F8EDE3]">
-            {/* <Header/> */}
+            <Header/>
             <ContentDaftarMenu/>
-            {/* <Footer/> */}
+            <Footer/>
         </div>
     )
 };
