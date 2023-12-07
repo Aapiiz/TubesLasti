f821e4f7dfbc08547febe7fdec3fc4ec0452f43d
Aapiiz <18221167@std.stei.itb.ac.id>
2023-12-07 15:38:55 +0700

    nyesuain file keseluruhan

--- TubesLastiCoyyy/src/pages/NoMeja.jsx
+++ TubesLastiCoyyy/src/pages/NoMeja.jsx
@@ -1,6 +1,6 @@
-import ContentNoMeja from "../component/contentNoMeja";
-import Footer from "../component/footer";
-import Header from "../component/Header"
+import ContentNoMeja from "../components/contentNoMeja";
+import Footer from "../components/Footer";
+import Header from "../components/Header";
 import React from "react";
 
 const NoMeja  = () => {
