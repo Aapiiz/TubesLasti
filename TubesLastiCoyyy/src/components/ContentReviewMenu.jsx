32fbe27046d00af5d7432f97d888e19568a7e81c
I Dewa Made Manu P <18221047@std.stei.itb.ac.id>
2023-12-08 17:06:38 +0700

    edit review menu

--- TubesLastiCoyyy/src/components/ContentReviewMenu.jsx
+++ TubesLastiCoyyy/src/components/ContentReviewMenu.jsx
@@ -12,43 +12,44 @@ const rupiah = (number) => {
   }).format(number);
 };
 
-const sumHarga = (menu) => {
-  let sum = 0;
-  for(let i = 0; i < menu.length; i++){
-    sum += (menu[i].harga * 1)
-  }
-  return sum;
-}
+// const sumHarga = (menu) => {
+//   let sum = 0;
+//   for(let i = 0; i < menu.length; i++){
+//     sum += (menu[i].harga * 1)
+//   }
+//   return sum;
+// }
 
 
 const ContentReviewMenu = () => {
   const navigate = useNavigate();
   const location = useLocation();
   console.log('location di review: ',location)
+  const [menuPesanan, setMenuPesanan] = useState([]);
   let idpesanan; 
-  let menu;
   try {
       idpesanan = location.state.idpesanan;    
-      menu = location.state.menu;
   } catch (error) {
       alert('anda harus memasukkan nama pemesan terlebih dahulu')
       window.location = '/'; //di login harus masukan nomor meja
   }
 
-  //getPesanan
-  const [menuPesanan, setMenuPesanan] = useState({});
+  
   useEffect(() => {
     Axios.get(`http://localhost:4000/pesanan/readpesananunique/${idpesanan}`)
-    .then(result => {
+    .then((result) => {
         console.log('data API', result.data); //Test hasil get
-        setMenuPesanan(result.data.data);
+        const responseAPI = result.data;
+        setMenuPesanan(new Array(responseAPI.data));
+        console.log('response data',responseAPI.data);
     })
     .catch(err => {
         console.log('error : ', err);
     })
-  }, [menuPesanan._id])
+  }, [idpesanan])
 
-
+  console.log('Type of menuPesanan:', typeof menuPesanan);
+  console.log(menuPesanan)
   // const [pesanan, setPesanan] = useState({});
   // useEffect(() => {
   //   Axios.get('http://localhost:4000/pesanan/readpesanan')
@@ -112,20 +113,20 @@ const handlePesan = () =>{
       {/* Berisi menu yang dipesan */}
       <div className="mt-4 flex justify-center">
         <div className="rounded-[30px] h-[400px] w-[1024px] bg-[#DAC0A3] overflow-y-auto">
-          {/* iterasi card pesanan sebanyak data yang ada di database*/}
-          {menu.map((pesanan) => (
-            <CardPesanan key={pesanan._id} namamenu={pesanan.namamenu} harga={pesanan.harga} jumlah={1}></CardPesanan>
-          ))
-
-          }
-          {/* {menuPesanan.map((pesanan) => (
+          <div className="flex justify-between">
+            <h1 className="ml-[30px] mt-5 text-2xl font-bold">Nama Pemesan:</h1>
+              {menuPesanan.map((pesanan) => (
+              <h1 key={pesanan._id} className="mr-[30px] mt-5 text-2xl font-bold">{pesanan.namapemesan}</h1>
+            ))}
+          </div>
+          {menuPesanan.map((pesanan) => (
             pesanan.listmenu.map((menu) => (
                 // console.log(menu),
                 // console.log(menu.namatenant),
                 // console.log(totalHarga),
                 <CardPesanan key={pesanan._id} namamenu={menu.namamenu} harga={menu.harga} jumlah={menu.jumlah}></CardPesanan>
             )))
-          )} */}
+          )}
           {/* total pembayaran */}
           <div className="relative flex py-5 items-center mx-[30px]">
             <div className="flex-grow border-t border-gray-400"></div>
@@ -134,9 +135,9 @@ const handlePesan = () =>{
             <h1 className="ml-[30px] text-2xl font-bold">Total Pembayaran</h1>
             {/* {menuPesanan[0] && menuPesanan[0].hargatotal ? rupiah(menuPesanan[0].hargatotal) : "Loading..."} */}
             {/* {console.log("isi: ", menuPesanan)} */}
-            {/* {menu.map((pesanan) => ( */}
-              <h1 className="mr-[30px] text-2xl font-bold">{rupiah(sumHarga(menu))}</h1>
-            {/* ))} */}
+            {menuPesanan.map((pesanan) => (
+              <h1 key={pesanan._id} className="mr-[30px] text-2xl font-bold">{rupiah(pesanan.hargatotal)}</h1>
+            ))}
             {/* <h1 className="mr-[30px] text-2xl font-bold">{rupiah(menuPesanan[0].hargatotal)}</h1> */}
           </div>
         </div>
@@ -147,11 +148,12 @@ const handlePesan = () =>{
         <button onClick={handlePesan} className="rounded-[20px] h-auto w-[300px] self text-center text-2xl text-[#FFFFFF] font-thin bg-[#BCD8B7] hover:bg-[#a7c1a3]">
           Pesan
         </button>
+        <button onClick={() => navigate(`/daftarmenu/`,{state: {namapemesan: menuPesanan[0].namapemesan}})}className="rounded-[20px] h-auto w-[300px] ml-5 self text-center text-2xl text-[#FFFFFF] font-thin bg-[#d0d492] hover:bg-[#bcb185]">
+          Kembali
+        </button>
       </div>
     </div>
   );
 };
-
-//onClick={() => navigate(`/halamanpembayaran/${pesanan._id}`, {state: {nomormeja: nomormeja}})}
 
 export default ContentReviewMenu;
