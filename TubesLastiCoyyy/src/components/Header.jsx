b43b3c05394825170b195c5e7aca67ad0f54d263
I Dewa Made Manu P <18221047@std.stei.itb.ac.id>
2023-12-09 08:22:05 +0700

    edit header

--- TubesLastiCoyyy/src/components/Header.jsx
+++ TubesLastiCoyyy/src/components/Header.jsx
@@ -1,8 +1,12 @@
+import { useNavigate } from "react-router-dom"
 
 const Header = () => {
+    const navigate = useNavigate();
     return (
         <div className ='flex justify-between items-center h-14 max-w-[1920px] py-10 px-4 bg-[#F8F0E5]'>
-            <img src="../images/Logo.png" alt="Logo Perusahaan" className="flex justify-center w-1/12 text-base font-bold text-[#DAC0A3]" />
+              <a onClick={() => navigate(`/`)} className="cursor-pointer">
+                <img src="../src/images/Logo.png" alt="Logo Perusahaan" className="flex justify-center w-16 text-base font-bold text-[#DAC0A3]" />
+              </a>
             {/* <h1 className ='flex justify-center w-full text-base font-bold text-[#DAC0A3]'>Ini entar diisi logo</h1> */}
         </div>
     )
